28c87f22d6c8760fe10675313255a4ab08b2850e
sureshgit001 <anthatisureshgoud55@gmail.com>
2023-09-30 23:39:06 +0530

    updated login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import RegisterLogIn from './SignUp_SignIn/RegisterLogIn';
+import { useEffect, useState } from 'react';
+import { ClipLoader } from 'react-spinners';
+import Success from './SignUp_SignIn/Success';
+
+
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      setData(<RegisterLogIn />)
+      setLoading(true)
+    }, 800);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+
+      {loading ?<Routes>
+          <Route path='/' element={<RegisterLogIn />} />
+          <Route path='/success' element={<Success />} />
+        </Routes>||data : <div className='loadingJs'>
+        <ClipLoader color="#36d7b7" size={50} />
+      </div>}
+    {/* <button ><Link to={'/success'}>success</Link></button> */}
+
+    </>
   );
 }
 
